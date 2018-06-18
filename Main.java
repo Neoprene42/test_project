cc2a8769daa27ad0087ff5ac2b1381fa9d80393e
Cameron <cameron.mcpherson@protonmail.com>
2018-06-18 19:10:42 +1000

    test

--- Main.java
+++ Main.java
@@ -8,15 +8,15 @@ public class Main {
 
         // ASSUMES NUMBER AT POSITION [0] IE -34 IS SORTED, THEN COMPARES THE NEXT ELEMENT ALONG TO THIS VALUE AND SWAPS IT IF IT'S SMALLER
         //SORTED PARTITION GROWS FROM LEFT
-            for (int firsUnsortedIndex = 1; firsUnsortedIndex < intArray.length; firsUnsortedIndex++){
-                int newElement = intArray[firsUnsortedIndex];
-                int i;
+        for (int firsUnsortedIndex = 1; firsUnsortedIndex < intArray.length; firsUnsortedIndex++){
+            int newElement = intArray[firsUnsortedIndex];
+            int i;
 
-                //THIS LOOP CONTINUES WHILE THE FIRST UNSORTED ELEMENT IS SMALLER THAN THE LAST SORTED ELEMENT
-                for (i= firsUnsortedIndex; i>0 && intArray[i-1] > newElement; i--){
-                    //THIS SWAPS THE TWO ELEMENTS TO MOVE THE SMALLER NUMBER TO THE LEFT, BUT OVERWRITES THE ORIGINAL VALUE
-                    //THIS ALLOWS SMALLER NUMBERS TO MOVE TO THE LEFT
-                    intArray[i] = intArray[i-1];
+            //THIS LOOP CONTINUES WHILE THE FIRST UNSORTED ELEMENT IS SMALLER THAN THE LAST SORTED ELEMENT
+            for (i= firsUnsortedIndex; i>0 && intArray[i-1] > newElement; i--){
+                //THIS SWAPS THE TWO ELEMENTS TO MOVE THE SMALLER NUMBER TO THE LEFT, BUT OVERWRITES THE ORIGINAL VALUE
+                //THIS ALLOWS SMALLER NUMBERS TO MOVE TO THE LEFT
+                intArray[i] = intArray[i-1];
             }
             //THIS OCCURS WHEN THE ELEMENT BEING CHECKED HAS REACHED THE FAR LEFT OF THE SORTED PARTITION, OR HAS REACHED A NUMBER SMALLER THAN IT
             //IT IS THEN REPLACED WITH ITSELF (IS WAS STORED AS 'NEW ELEMENT' INITIALLY
